ba0b47febd5bcd53888c20630373bf053744b4c3
Menma <menma@menmasystems.com>
2025-06-25 19:30:32 +0300

    Fix terraform architecture for lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "menmaportfolio" {
   image_uri     = var.image_uri
   timeout       = 30
   memory_size   = 512
-  architectures = ["amd64"]
+  architectures = ["x86_64"]
 }
 
 resource "aws_apigatewayv2_api" "http_api" {
