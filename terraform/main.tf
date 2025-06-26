3130c7c68944eead6a461f88fff72916a94a736f
Menma <menma@menmasystems.com>
2025-06-26 19:49:56 +0300

    Update terraform plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,11 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
+  role = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "menmaportfolio" {
   function_name = "MenmaPortfolioFunction"
   role          = aws_iam_role.lambda_exec.arn
