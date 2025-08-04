27d97550b769e4830a990adc0262ae7f87a6592f
Menma <menma@menmasystems.com>
2025-08-04 16:49:55 +0300

    [Terraform] Update Cloudfront cache policy to ManagedCaching

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -166,7 +166,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
     target_origin_id       = local.cf_origin_id
     viewer_protocol_policy = "redirect-to-https"
 
-    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    cache_policy_id          = "658327ea-f89d-4fab-a63d-7e88639e58f6"
     origin_request_policy_id = aws_cloudfront_origin_request_policy.origin_request_policy.id
   }
 
